fe5a1f1c0ffbb89e9d28a158905dc55462afad6f
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-03-29 02:04:11 +0700

    update songs/sound_effect.py

--- songs/sound_effect.py
+++ songs/sound_effect.py
@@ -1,18 +1,34 @@
-import random 
-import os 
-num=random.randint(1,3)
-print(num)
-os.system("clear")
-print("\033[1m\033[33m[\033[32m─────────────────Booting Termux Terminal─────────────────\033[33m]\033[0m")
+import os
+import sys
 
+def play_sound(option):
+    sounds = {
+        "1": "process.mp3",
+        "2": "granted.mp3",
+        "3": "denied.mp3"
+    }
+    
+    if option not in sounds:
+        print("Invalid option. Use 1, 2, or 3.")
+        sys.exit(1)
+    
+    os.system("clear")
+    print(
+        "\033[1;32m──────────────── Booting Termux Terminal ────────────────\033[0m"
+    )
+    os.system(
+        f"nohup mpv ~/Termux-Custom/songs/{sounds[option]} > /dev/null 2>&1 &"
+    )
 
-# Condition 
-
-if num == 1:
-    os.system("mpv Access-Granted.mp3")
-elif num==2:
-    os.system("mpv Jarvis2.mp3")
-else :
-    os.system("mpv JARVIS.mp3")
- 
-###########################################
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(
+            "Example:\n"
+            "- python3 sound_effect.py 1 (Process)\n"
+            "- python3 sound_effect.py 2 (Granted)\n"
+            "- python3 sound_effect.py 3 (Denied)\n"
+            ""
+        )
+        sys.exit(1)
+    
+    play_sound(sys.argv[1])
